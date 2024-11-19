0a49ab4e7e94649cb4925eccbb2fa6675380d7cf
zabedhasan129 <hasanalzabed2013@gmail.com>
2024-11-20 00:33:34 +0600

    routesSetup

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,18 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const secureApi = require("./middleware/secureApi")
+const routes = require("./routes")
+
 
  
-app.use(cors())
-
+app.use(cors());
 app.use(express.json());
-app.post("/api/v1/auth/registration",secureApi,(req,res)=>{
-    console.log(req.body);
-})
+app.use(routes)
+
+
+// app.post("/api/v1/auth/registration",secureApi,(req,res)=>{
+//     console.log(req.body);
+// })
 
 app.listen(8000,()=>{
     console.log("port connected")
